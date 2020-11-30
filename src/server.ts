636a650211877a1def6c26c521221163d65431cd
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-11-30 18:52:14 +0200

    TODO1 completed: GET /filteredimage?image_url={{}} endpoint

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,6 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
@@ -28,8 +27,20 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get('/filteredimage', async (req, res) => {
+    const image_url = req.query.image_url
+    if (!image_url) {
+      res.status(400).send("image_url query does not exist")
+    }
 
-  //! END @TODO1
+    const image_file = await filterImageFromURL(image_url)
+
+    res.sendFile(image_file)
+
+    res.on('finish', () => {
+      deleteLocalFiles()
+    })
+  })
 
   // Root Endpoint
   // Displays a simple message to the user
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import path from 'path';
 import Jimp = require('jimp');
 
 // filterImageFromURL
@@ -8,17 +9,17 @@ import Jimp = require('jimp');
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
-export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+    return new Promise(async resolve => {
         const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
+        const outpath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
         await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
+            .resize(256, 256) // resize
+            .quality(60) // set JPEG quality
+            .greyscale() // set greyscale
+            .write(path.join(__dirname, outpath), (img) => {
+                resolve(path.join(__dirname, outpath));
+            });
     });
 }
 
@@ -27,8 +28,12 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files:Array<string>){
-    for( let file of files) {
+export async function deleteLocalFiles() {
+    const temp_dir_path = path.join(__dirname, '/tmp/')
+    const files = fs.readdirSync(temp_dir_path)
+        .map(p => temp_dir_path + p)
+
+    for (let file of files) {
         fs.unlinkSync(file);
     }
 }
